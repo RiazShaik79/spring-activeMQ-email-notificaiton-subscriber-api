040b4c96cc2b11f73d690a536225ca930defae3f
riaz.shaik <riaz.shaik@CPX-6ZN7OQLNAAO.dir.svc.accenture.com>
2020-05-03 23:20:05 +0100

    update to send email to user

--- src/main/java/io/javabrains/Receiver.java
+++ src/main/java/io/javabrains/Receiver.java
@@ -1,14 +1,25 @@
 package io.javabrains;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
+	@Autowired
+	private EmailService emailService;
+	
   @JmsListener(destination = "mailbox", containerFactory = "myFactory")
   public void receiveMessage(Email email) {
+	  
     System.out.println("Received <" + email + ">");
+    try {
+    emailService.sendSimpleMessage(email.getTo(), email.getSubject(), email.getBody());
+    } catch(Exception e) {
+    	System.out.println("Email Exception : " + e);
+    }
+    
   }
 
 }
